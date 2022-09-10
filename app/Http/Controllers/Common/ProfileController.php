455b1aab753ac3caa695f86a56166ce2bea5e95b
deepika <deepika.manifest@gmail.com>
2022-09-10 15:54:35 +0530

    bug fix

--- app/Http/Controllers/Common/ProfileController.php
+++ app/Http/Controllers/Common/ProfileController.php
@@ -20,14 +20,16 @@ class ProfileController extends Controller
 
     public function update_profile(Request $request)
     {
-        $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
         $user = Auth::guard('api')->user();
         $c = Company::where('user_id', $user->id)->first();
+        return response(["status" => "success", 'res' => $c], 200);
+
+        $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
         $validator = Validator::make($request->all(), [
             'first_name' => 'required|max:255',
             'last_name' => 'required|max:255',
             'company_name' => 'required|max:255',
-            'company_domain' =>  'required|max:255|regex:' . $regex . '|unique:companies,company_domain,'. $c->id,
+            'company_domain' =>  'required|max:255|regex:' . $regex . '|unique:companies,company_domain,'.$c->id, 
         ]);
         if ($validator->fails()) {
             $error = $validator->getMessageBag()->first();
