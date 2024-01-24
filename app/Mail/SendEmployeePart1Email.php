3608fe8c706186e538db98e09a696b2ef32ec479
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2024-01-24 11:20:24 +0530

    email part1

---
+++ app/Mail/SendEmployeePart1Email.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmployeePart1Email extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $maildata;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($maildata)
+    {
+        $this->maildata = $maildata;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.sendPart1Email')
+            ->subject('Complete part1 learning plan')
+            ->with('maildata', $this->maildata);
+    }
+}
