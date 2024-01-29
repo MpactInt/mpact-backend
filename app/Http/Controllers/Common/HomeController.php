400ad36966631f1a8e900d6c299a1e4508dc90fc
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2024-01-30 01:40:42 +0530

    email changes

--- app/Http/Controllers/Common/HomeController.php
+++ app/Http/Controllers/Common/HomeController.php
@@ -590,8 +590,10 @@ class HomeController extends Controller
         //         ->subject('Welcome to Mpact International');
         //     $message->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
         // });
-        $maildata = ['name' => "test c"];
+        $maildata = array('link' => 'this is link', 'name' => 'firstname', 'text' => 'You can use below link to create your password', 'link_text' => 'Click to create your password');
 
-        Mail::to("deepika.manifest@gmail.com")->send(new SendRegistrationEmail($maildata));
+        Mail::to("nchouksey@manifestinfotech.com")->send(new SendEmployeeRegistrationEmail($maildata));
+
+                            
     }
 }
