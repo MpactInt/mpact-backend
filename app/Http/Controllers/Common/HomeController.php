823001200416785917d22d127316065afbb5dab2
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2024-02-07 21:27:08 +0530

    email changes

--- app/Http/Controllers/Common/HomeController.php
+++ app/Http/Controllers/Common/HomeController.php
@@ -592,22 +592,20 @@ class HomeController extends Controller
         // });
         
 
-        //$link = env('FRONT_URL') . '/login';
-        //$maildata = array('name' => 'Maisha', 'link' => $link);
+        $link = env('FRONT_URL') . '/login';
+        $maildata = array('name' => 'Maisha', 'link' => $link);
+        Mail::to("maisha@mpact-int.com")->send(new SendEmployeePart1Email($maildata));
         //Mail::to("nchouksey@manifestinfotech.com")->send(new SendEmployeePart1Email($maildata));
-        //Mail::to("maisha@mpact-int.com")->send(new SendEmployeePart1Email($maildata));
 
-        //$link = md5(uniqid());
-        //$link1 = env('FRONT_URL') . '/create-password/' . $link;
-        //$maildata = array('link' => $link1, 'name' => 'first name', 'text' => 'You can use below link to create your password', 'link_text' => 'Click to create your password');
-        //Mail::to("maisha@mpact-int.com")->send(new SendEmployeeRegistrationEmail($maildata));
+        $link = md5(uniqid());
+        $link1 = env('FRONT_URL') . '/create-password/' . $link;
+        $maildata = array('link' => $link1, 'name' => 'first name', 'text' => 'You can use below link to create your password', 'link_text' => 'Click to create your password');
+        Mail::to("maisha@mpact-int.com")->send(new SendEmployeeRegistrationEmail($maildata));
+        //Mail::to("nchouksey@manifestinfotech.com")->send(new SendEmployeePart1Email($maildata));
 
-        //$maildata = ['name' => 'test company'];
-        //Mail::to("maisha@mpact-int.com")->send(new SendRegistrationEmail($maildata));
+        $maildata = ['name' => 'test company'];
+        Mail::to("maisha@mpact-int.com")->send(new SendRegistrationEmail($maildata));
         //Mail::to("nchouksey@manifestinfotech.com")->send(new SendRegistrationEmail($maildata));
-
-        //$maildata = array('link' => "this is link", 'text' => 'You can use below link to reset your password, this link will be expired in 10 min', 'link_text' => 'Click to reset your password');
-        //Mail::to("maisha@mpact-int.com")->send(new ForgotPasswordEmail($maildata));
 
         $link = md5(uniqid());
         $link1 = env('FRONT_URL') . '/reset-password/' . $link;
