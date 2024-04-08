f27d2173a8800d0e99d9e306df25ef6d84c34929
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2024-04-08 22:10:39 +0530

    email testing

--- app/Console/Commands/PartLearningPlanEmailCrone.php
+++ app/Console/Commands/PartLearningPlanEmailCrone.php
@@ -50,7 +50,10 @@ class PartLearningPlanEmailCrone extends Command
      */
     public function handle()
     {
-         \Log::info("PartLearningPlanEmailCrone:done");
+        
+      
+      try {
+          
 
 
         $link = env('FRONT_URL') . '/employee/my-learning-plan/';
@@ -58,7 +61,9 @@ class PartLearningPlanEmailCrone extends Command
 
         //Mail::to("aakashsingh624@gmail.com")->send(new SendGeneralPartLearningPlanEmail($maildata));
         Mail::to("nchouksey@manifestinfotech.com")->send(new SendGeneralPartLearningPlanEmail($maildata));
-
+      } catch (Exception $e) {
+           \Log::info($e);
+      }
         // $learning_plans_today = MyLearningPlan::select('my_learning_plans.id', 'my_learning_plans.title', 'my_learning_plans.email_subject', 'my_learning_plans.email_body', 'users.email','company_employees.first_name','company_employees.last_name', 'user_learning_plans.learning_plan_enable_date',)
         //     ->join('user_learning_plans', 'my_learning_plans.id', 'user_learning_plans.learning_plan_id')
         //     ->join('users', 'users.id', 'user_learning_plans.user_id')
