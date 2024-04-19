37c7cdc9aa4e865eb9c0ac96868bb41f6f1ba66a
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2024-04-20 00:04:52 +0530

    email testing

--- app/Mail/SendGeneralPartLearningPlanEmail.php
+++ app/Mail/SendGeneralPartLearningPlanEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class SendGeneralPartLearningPlanEmail extends Mailable
+class SendGeneralPartLearningPlanEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $maildata;
