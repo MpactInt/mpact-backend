288427d98dd33600d98ac2cbfc2f8e310dae3d5e
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2024-05-06 18:28:07 +0530

    Tutorial and login reminder emails

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\SendCheckinSurveyEmail::class,
-        Commands\PartLearningPlanEmailCrone2::class,
         Commands\LoginReminderEmail::class
     ];
 
